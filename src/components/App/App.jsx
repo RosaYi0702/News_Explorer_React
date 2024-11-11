04a4912da3d0da5dd8334e9587ae470857551f65
RosaYi <rosayihuilin@gmail.com>
2024-11-10 20:29:50 -0500

    log in header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,10 +7,13 @@ import Footer from "../Footer/Footer";
 import LogInModal from "../LogInModal/LogInModal";
 import RegisterModal from "../RegisterModal/RegisterModal";
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 
 function App() {
   const [activeModal, setActiveModal] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [currentUser, setCurrentUser] = useState([]);
 
   const handleCloseModal = () => {
     setActiveModal("");
@@ -25,28 +28,44 @@ function App() {
   };
 
   return (
-    <div className="page">
-      <div className="page__content">
-        <div className="page__home">
-          <Header handleSignInModal={handleSignInModal} />
-          <SearchPage />
+    <CurrentUserContext.Provider
+      value={{ isLoggedIn, setIsLoggedIn, currentUser }}
+    >
+      <div className="page">
+        <div className="page__content">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div className="page__home">
+                    <Header
+                      handleSignInModal={handleSignInModal}
+                      isLoggedIn={isLoggedIn}
+                    />
+                    <SearchPage />
+                  </div>
+                  <SearchResult />
+                  <AboutAuthor />
+                </>
+              }
+            ></Route>
+          </Routes>
+          <Footer />
         </div>
-        <SearchResult />
-        <AboutAuthor />
-        <Footer />
-      </div>
 
-      <LogInModal
-        isOpened={activeModal === "log-in"}
-        handleCloseModal={handleCloseModal}
-        handleSwitchModal={handleSignUpModal}
-      />
-      <RegisterModal
-        isOpened={activeModal === "register"}
-        handleCloseModal={handleCloseModal}
-        handleSwitchModal={handleSignInModal}
-      />
-    </div>
+        <LogInModal
+          isOpened={activeModal === "log-in"}
+          handleCloseModal={handleCloseModal}
+          handleSwitchModal={handleSignUpModal}
+        />
+        <RegisterModal
+          isOpened={activeModal === "register"}
+          handleCloseModal={handleCloseModal}
+          handleSwitchModal={handleSignInModal}
+        />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,12 @@
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/WhiteLogo.png";
+import logOut from "../../assets/logout.png";
 
-function Header({ handleSignInModal }) {
+function Header({ handleSignInModal, isLoggedIn }) {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <header className="header">
       <nav className="header__nav">
@@ -11,9 +15,25 @@ function Header({ handleSignInModal }) {
         </Link>
         <div className="header__btn">
           <button className="header__home-btn">Home</button>
-          <button className="header__sign-in-btn" onClick={handleSignInModal}>
-            Sign In
-          </button>
+          {isLoggedIn ? (
+            <>
+              <button className="header__saved-articles-btn">
+                Saved articles
+              </button>
+              <Link to="/saved-news" className="header__log-out">
+                <p className="header__user-name">Elisa</p>
+                <img
+                  src={logOut}
+                  alt="Log Out icon"
+                  className="header__log-out_img"
+                />
+              </Link>
+            </>
+          ) : (
+            <button className="header__sign-in-btn" onClick={handleSignInModal}>
+              Sign In
+            </button>
+          )}
         </div>
       </nav>
     </header>
